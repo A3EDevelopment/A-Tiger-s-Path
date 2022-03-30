aad144e4d0ee6de42dce94a57ec805869d2d1029
ChoppedSpam <97447876+ChoppedSpam@users.noreply.github.com>
2022-03-30 10:43:22 +0100

    Gliding Update
    Found out problems that subsided with the gliding mechanic and added moving platforms

--- Assets/Starter/Gliding.cs
+++ Assets/Starter/Gliding.cs
@@ -6,9 +6,15 @@ public class Gliding : MonoBehaviour
 {
     public LayerMask whatIsGround;
     public GameObject groundChecker;
-    public bool isOnGround;
     public GameObject Player;
+
+    public Transform myEndPoint;
+    public Transform playerTransform;
+
+    public bool isOnGround;
     public float glidemass;
+    public float speed;
+
     Rigidbody myRigidbody;
 
     public void Start()
@@ -20,10 +26,13 @@ public class Gliding : MonoBehaviour
     {
         isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, whatIsGround);
 
-        if (isOnGround == false && Input.GetKey(KeyCode.F))
+        if (/*isOnGround == false && */Input.GetKey(KeyCode.F))
         {
             //Player.transform.Translate(Vector3.up * glidemass, Space.World);
-            Player.transform.position +=  new Vector3(0.0f, glidemass, 0.0f);
+            //Player.transform.position +=  new Vector3(0.0f, glidemass, 0.0f);
+            playerTransform.transform.position = Vector3.MoveTowards(playerTransform.position, myEndPoint.position, speed);
+            //playerTransform.position = myEndPoint.position;
+            //Debug.Log ("yo");
             //myRigidbody.AddForce(new Vector3(0.0f, glidemass, 0.0f));
         }
     }

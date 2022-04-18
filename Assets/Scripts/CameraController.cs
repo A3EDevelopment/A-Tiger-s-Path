6757d76ce5efd2ca580f070e4d3a42e463486d33
ChoppedSpam <97447876+ChoppedSpam@users.noreply.github.com>
2022-04-18 14:35:49 +0100

    Materials added
    Touched up on dialogue mechanic, camera locks and more testing with textures of the buildings.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using static Models;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("References")]
+    public Basic playerController;
+    private Vector3 targetRotation;
+    public GameObject yGimbal;
+    private Vector3 yGibalRotation;
+
+    [Header("Settings")]
+    public CameraSettingsModel settings;
+    public float movementSmoothTime;
+    private Vector3 movementVelocity;
+
+    #region - Update -
+    private void Update()
+    {
+
+        FollowPlayerCameraTarget();
+        CameraRotation();
+
+    }
+    #endregion
+
+    #region - Position / Rotation -
+    private void CameraRotation()
+    {
+        var viewInput = playerController.input_View;
+
+        targetRotation.y += (settings.InvertedX ? -(viewInput.x * settings.SensitivityX) : (viewInput.x * settings.SensitivityX)) * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(targetRotation);
+
+        //Turns Vector into Quaternion for Rotation
+
+        yGibalRotation.x += (settings.InvertedY ? (viewInput.y * settings.SensitivityY) : -(viewInput.y * settings.SensitivityY)) * Time.deltaTime;
+        yGibalRotation.x = Mathf.Clamp(yGibalRotation.x, settings.YClampMin, settings.YClampMax);
+
+        //Looking Up and Down. 
+        //Looking Left and Right.
+        //Question mark is used like an if statement, first value is true
+
+        yGimbal.transform.localRotation = Quaternion.Euler(yGibalRotation);  
+        
+        //Only changes y axis, not x
+
+        if (playerController.isTargetMode)
+        {
+            var currentRotation = playerController.transform.rotation;
+
+            var newRotation = currentRotation.eulerAngles;
+            newRotation.y = targetRotation.y;
+
+            currentRotation = Quaternion.Lerp(currentRotation, Quaternion.Euler(newRotation), settings.CharacterRotationSpeedSmoothdamp);
+
+            playerController.transform.rotation = currentRotation;
+        }
+    }
+
+    private void FollowPlayerCameraTarget()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, playerController.cameraTarget.position, ref movementVelocity, movementSmoothTime);
+
+    }
+    #endregion
+
+}
